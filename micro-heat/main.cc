db673cc51e1abac9cbe04251aa1939146298fbd1
Helena Kschidock <h.kschidock@gmail.com>
2023-01-11 14:08:52 +0100

    Init from dumux-phasefield/examples/plainallencahn

---
+++ micro-heat/main.cc
@@ -0,0 +1,196 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+
+// based on dumux-phasefield/examples/plainallencahn
+#include <config.h>
+
+#include <ctime>
+#include <iostream>
+#include <fstream>
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/timer.hh>
+#include <dune/grid/io/file/vtk.hh>
+#include <dune/istl/io.hh>
+
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/dumuxmessage.hh>
+#include <dumux/common/defaultusagemessage.hh>
+
+#include <dumux/linear/seqsolverbackend.hh>
+#include <dumux/nonlinear/newtonsolver.hh>
+
+#include <dumux/assembly/fvassembler.hh>
+#include <dumux/assembly/diffmethod.hh>
+
+#include <dumux/discretization/method.hh>
+#include <dumux/io/grid/gridmanager.hh>
+#include <dumux/io/vtkoutputmodule.hh>
+
+#include "properties.hh"
+
+void usage(const char *progName, const std::string &errorMsg)
+{
+    if (errorMsg.size() > 0) {
+        std::string errorMessageOut = "\nUsage: ";
+                    errorMessageOut += progName;
+                    errorMessageOut += " [options]\n";
+                    errorMessageOut += errorMsg;
+                    errorMessageOut += "\n\nThe list of important arguments for this program is:\n"
+                            "\t-TimeLoop.TEnd             end time of the simulation\n"
+                            "\t-TimeLoop.DtInitial        initial time step size\n"
+                            "\t-TimeLoop.MaxTimeStepSize  maximal time step size\n"
+                            "\t-Grid.LowerLeft            lower left (front) corner of the domain\n"
+                            "\t-Grid.UpperRight           upper right (back) corner of the domain\n"
+                            "\t-Grid.Cells                grid resolution in each coordinate direction\n"
+                            "\t-Problem.xi                phasefield parameter\n"
+                            "\t-Problem.omega             phasefield diffusivity/surface tension parameter\n"
+                            "\t-Problem.delta             regularization parameter\n"
+                            "\t-Problem.OutputInterval    interval size for VTK output\n"
+                            "\t-Problem.Name              base name for VTK output files\n";
+        std::cout << errorMessageOut
+                  << "\n";
+    }
+}
+
+int main(int argc, char** argv)
+{
+    using namespace Dumux;
+
+    // define the type tag for this problem
+    using TypeTag = Properties::TTag::PlainAllenCahn;
+
+    // initialize MPI, finalize is done automatically on exit
+    const auto& mpiHelper = Dune::MPIHelper::instance(argc, argv);
+
+    // print dumux start message
+    if (mpiHelper.rank() == 0)
+        DumuxMessage::print(/*firstCall=*/true);
+
+    // parse command line arguments and input file
+    Parameters::init(argc, argv);
+
+    // try to create a grid (from the given grid file or the input file)
+    GridManager<GetPropType<TypeTag, Properties::Grid>> gridManager;
+    gridManager.init();
+
+    ////////////////////////////////////////////////////////////
+    // run instationary non-linear problem on this grid
+    ////////////////////////////////////////////////////////////
+
+    // we compute on the leaf grid view
+    const auto& leafGridView = gridManager.grid().leafGridView();
+
+    // create the finite volume grid geometry
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    auto gridGeometry = std::make_shared<GridGeometry>(leafGridView);
+    gridGeometry->update(leafGridView);
+
+    // the problem (initial and boundary conditions)
+    using Problem = GetPropType<TypeTag, Properties::Problem>;
+    auto problem = std::make_shared<Problem>(gridGeometry);
+
+    // get some time loop parameters
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    const auto tEnd = getParam<Scalar>("TimeLoop.TEnd");
+    const auto maxDt = getParam<Scalar>("TimeLoop.MaxTimeStepSize");
+    auto dt = getParam<Scalar>("TimeLoop.DtInitial");
+
+    // the solution vector
+    using SolutionVector = GetPropType<TypeTag, Properties::SolutionVector>;
+    auto xPtr = std::make_shared<SolutionVector>();
+    SolutionVector x = *xPtr;
+    problem->applyInitialSolution(x);
+    auto xOldPtr = std::make_shared<SolutionVector>();
+    *xOldPtr = x;
+
+    // the grid variables
+    using GridVariables = GetPropType<TypeTag, Properties::GridVariables>;
+    auto gridVariables = std::make_shared<GridVariables>(problem, gridGeometry);
+    gridVariables->init(x);
+
+    // intialize the vtk output module
+    using IOFields = GetPropType<TypeTag, Properties::IOFields>;
+    VtkOutputModule<GridVariables, SolutionVector> vtkWriter(*gridVariables, x, problem->name());
+    IOFields::initOutputModule(vtkWriter); //!< Add model specific output fields
+    vtkWriter.write(0.0);
+
+    // instantiate time loop
+    auto timeLoop = std::make_shared<CheckPointTimeLoop<Scalar>>(0.0, dt, tEnd);
+    timeLoop->setMaxTimeStepSize(maxDt);
+
+    bool enableCheckPoints = hasParam("Problem.OutputInterval");
+    if (enableCheckPoints)
+        timeLoop->setPeriodicCheckPoint(getParam<double>("Problem.OutputInterval"));
+
+    // the assembler with time loop for instationary problem
+    using Assembler = FVAssembler<TypeTag, DiffMethod::numeric>;
+    auto assembler = std::make_shared<Assembler>(problem, gridGeometry, gridVariables, timeLoop, *xOldPtr);
+
+    // the linear solver
+    using LinearSolver = UMFPackBackend;
+    auto linearSolver = std::make_shared<LinearSolver>();
+
+    // the non-linear solver
+    using NewtonSolver = Dumux::NewtonSolver<Assembler, LinearSolver>;
+    auto nonLinearSolver = std::make_shared<NewtonSolver>(assembler, linearSolver);
+
+    // time loop
+    timeLoop->start(); do
+    {
+        // linearize & solve
+        problem->updateConservationSource(*assembler, x);
+        nonLinearSolver->solve(x, *timeLoop);
+
+        // make the new solution the old solution
+        *xOldPtr = x;
+        gridVariables->advanceTimeStep();
+
+        // advance to the time loop to the next step
+        timeLoop->advanceTimeStep();
+
+        // report statistics of this time step
+        timeLoop->reportTimeStep();
+
+        // set new dt as suggested by the newton solver
+        timeLoop->setTimeStepSize(nonLinearSolver->suggestTimeStepSize(timeLoop->timeStepSize()));
+
+        if (timeLoop->isCheckPoint() || timeLoop->finished() || !enableCheckPoints)
+        {
+            vtkWriter.write(timeLoop->time());
+        }
+
+    } while (!timeLoop->finished());
+
+    timeLoop->finalize(leafGridView.comm());
+
+    ////////////////////////////////////////////////////////////
+    // finalize, print dumux message to say goodbye
+    ////////////////////////////////////////////////////////////
+
+    // print dumux end message
+    if (mpiHelper.rank() == 0)
+    {
+        Parameters::print();
+        DumuxMessage::print(/*firstCall=*/false);
+    }
+
+    return 0;
+}
---
+++ micro-heat/problem.hh
@@ -0,0 +1,190 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+#ifndef DUMUX_PLAINALLENCAHN_PROBLEM_HH
+#define DUMUX_PLAINALLENCAHN_PROBLEM_HH
+
+#include <dumux/common/fvproblemwithspatialparams.hh>
+#include <dumux/common/timeloop.hh>
+#include <dune/istl/matrix.hh>
+#include <fstream>
+#include <iostream>
+
+namespace Dumux {
+
+template <class TypeTag >
+class PlainAllenCahnProblem : public FVProblemWithSpatialParams<TypeTag>
+{
+    using ParentType = FVProblemWithSpatialParams<TypeTag>;
+    using GridView = typename GetPropType<TypeTag, Properties::GridGeometry>::GridView;
+    using FVGridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using Indices = typename GetPropType<TypeTag, Properties::ModelTraits>::Indices;
+    using BoundaryTypes = Dumux::BoundaryTypes<GetPropType<TypeTag,
+          Properties::ModelTraits>::numEq()>;
+    using PrimaryVariables = GetPropType<TypeTag, Properties::PrimaryVariables>;
+    using NumEqVector = Dumux::NumEqVector<PrimaryVariables>;
+    using ElementVolumeVariables = typename GetPropType<TypeTag, Properties::GridVolumeVariables>::LocalView;
+    using FVElementGeometry = typename GetPropType<TypeTag, Properties::GridGeometry>::LocalView;
+    using SubControlVolume = typename FVElementGeometry::SubControlVolume;
+    using SolutionVector = GetPropType<TypeTag, Properties::SolutionVector>;
+    using TimeLoopPtr = std::shared_ptr<CheckPointTimeLoop<Scalar>>;
+    using Matrix = Dune::FieldMatrix<Scalar,3,3>;
+    using Vector = Dune::FieldVector<Scalar,3>;
+
+    static constexpr int phiIdx = Indices::phiIdx;
+
+    using Element = typename GridView::template Codim<0>::Entity;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+
+public:
+    PlainAllenCahnProblem( std::shared_ptr<const FVGridGeometry> fvGridGeometry)
+    : ParentType(fvGridGeometry)
+    {
+        omega_ = getParam<Scalar>("Problem.omega");
+        alpha_ = 1.0;
+        if (getParam<Scalar>("Problem.UseAlpha"))
+        {
+            alpha_ = 1.0/omega_;
+            omega_ = 1.0;
+        }
+        xi_ = getParam<Scalar>("Problem.xi");
+    }
+
+    BoundaryTypes boundaryTypesAtPos(const GlobalPosition &globalPos) const
+    {
+        BoundaryTypes bcTypes;
+
+        bcTypes.setAllNeumann();
+
+        return bcTypes;
+    }
+
+    /*!
+     * \brief Calculates the derivative of the double-well potential P = 8 \phi^2 (1-\phi)^2.
+     */
+    Scalar pPrime(Scalar phi) const
+    {
+        return 16.0 * phi * (1.0 - phi) * (1.0 - 2.0*phi);
+    }
+
+    NumEqVector source(const Element &element,
+                       const FVElementGeometry& fvGeometry,
+                       const ElementVolumeVariables& elemVolVars,
+                       const SubControlVolume &scv) const
+    {
+        // declare source vector
+        NumEqVector source;
+        // extract priVars
+        const auto& priVars = elemVolVars[scv].priVars();
+
+        source[phiIdx] = -omega_ * pPrime(priVars[phiIdx]);
+        //source += conservationSource(fvGeometry, elemVolVars);
+        source += 4*xi_*priVars[phiIdx]*(1.0-priVars[phiIdx])
+            * interfaceVelocity(element, fvGeometry, elemVolVars, scv);
+        return source;
+    }
+
+    /*!
+     * \brief Returns the source term to enforce conservation of phase-field function calculated
+     *        by updateConservationSource
+     */
+    Scalar conservationSource(const FVElementGeometry& fvGeometry,
+                              const ElementVolumeVariables& elemVolVars) const
+    {
+        return conservationSource_;
+    }
+
+    /*!
+     * \brief Returns the interfaceVelocity to use in the source term
+     */
+    Scalar interfaceVelocity(const Element& element,
+                             const FVElementGeometry& fvGeometry,
+                             const ElementVolumeVariables& elemVolVars,
+                             const SubControlVolume& scv) const
+    {
+        return 0.0;
+    }
+
+    Scalar poreVolume() const
+    {
+        return poreVolume_;
+    }
+
+    /*!
+     * \brief Computes the expensive source term to enforce conservation of phase-field function
+     */
+    template <class Assembler, class SolutionVector>
+    void updateConservationSource(const Assembler& assembler,
+                                  const SolutionVector& curSol)
+    {
+        const auto& gridGeometry = this->gridGeometry();
+        auto elemGeometry = localView(gridGeometry);
+        const auto gridVolVars = assembler.gridVariables().curGridVolVars();
+        ElementVolumeVariables elemVolVars = localView(gridVolVars);
+        Scalar volume = 0;
+        Scalar integral = 0;
+        for (const auto& element : elements(gridGeometry.gridView()))
+        {
+            elemGeometry.bind(element);
+            elemVolVars.bind(element, elemGeometry, curSol);
+            for (const auto& scv : scvs(elemGeometry))
+            {
+                volume += scv.volume();
+                integral += pPrime(elemVolVars[scv].priVar(phiIdx)) * scv.volume();
+            }
+        }
+        poreVolume_ = volume;
+        conservationSource_ = omega_ * integral/volume;
+    }
+
+    PrimaryVariables initialAtPos(const GlobalPosition &globalPos) const
+    {
+        PrimaryVariables values;
+
+        const static Scalar centerX = getParam<Scalar>("Problem.CenterX");
+        const static Scalar centerY = getParam<Scalar>("Problem.CenterY");
+        const static Scalar radius = getParam<Scalar>("Problem.Radius");
+        const static Scalar factor = getParam<Scalar>("Problem.PhasefieldICScaling");
+        Scalar s = (globalPos[0]-centerX)*(globalPos[0]-centerX)
+            +(globalPos[1]-centerY)*(globalPos[1]-centerY)
+            - radius*radius;
+        values[phiIdx] = 1.0/(1.0 + std::exp(-factor*s/xi_));
+        return values;
+    }
+
+    Scalar getAlpha() const
+    {
+        return alpha_;
+    }
+
+    Scalar getOmega() const
+    {
+        return omega_;
+    }
+
+private:
+    Scalar xi_;
+    Scalar omega_;
+    Scalar alpha_;
+    Scalar poreVolume_, conservationSource_;
+};
+
+} //end namespace Dumux
+
+#endif // DUMUX_PLAINALLENCAHN_PROBLEM_HH
