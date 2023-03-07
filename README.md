9be4961a149fed83d9dc39af70aa2737c4907c77
Helena Kschidock <h.kschidock@gmail.com>
2023-03-07 10:32:56 +0100

    Setup pybind with Cmake and couple (compiles, crashes)

--- README.md
+++ README.md
@@ -4,21 +4,25 @@
 1. Follow the setup instructions of the [dumux-adapter](https://github.com/precice/dumux-adapter "dumux-adapter"). You folder structure should look something like this: `dumux/dumux-adapter/`.
 2. Navigate into `dumux-adapter/examples/` and git clone this repo into `dumux-adapter/examples/macro-micro`.
 3. Add the line `add_subdirectory(macro-micro)` to `dumux-adapter/examples/CMakeLists.txt`.
+4. Navigate into `examples/macro-micro/micro-heat` and clone pybind into a subdirectory by running `git clone https://github.com/pybind/pybind11`. For more information, see also [https://github.com/pybind/cmake_example](https://github.com/pybind/cmake_examples).
 
 ## Building the executables
-1. Rebuild the dumux-adapter by navigating into your `dumux` folder and executing `dunecontrol --only=dumux-adapter all`. Repeat this step any time you modify the CMake files.
+1. Rebuild the dumux-adapter by navigating into your `dumux` folder and executing `dunecontrol --only=dumux-adapter all`. Repeat this step any time you modify the CMake files or to rebuild the DuMuX micro executable.
 2. Build the nutils micro executable by navigating into `dumux/dumux-adapter/build-cmake/examples/macro-micro/nutils-micro-heat` and executing `make copy_micro_heat_python`. Repeat this step any time you modify the micro simulation files.
 3. Build the DuMuX macro executable by navigating into `dumux/dumux-adapter/build-cmake/examples/macro-micro/macro-heat` and executing `make test_macro_heat`. (By default, this uses [CCTPFA](https://dumux.org/docs/doxygen/releases/3.2/a00461_source.html) discretization. To use [Box discretization](https://dumux.org/docs/doxygen/releases/3.1/a02046.html), build `make test_macro_heat_box` instead.)
 
 ## Running the simulating
-### Running only the macro simulation
+### Running only the DuMuX macro simulation
 * Set the `RunWithCoupling ` parameter in `macro-heat/params.input`to `false`. Rebuild afterwards.
 * Navigate to `dumux/dumux-adapter/build-cmake/examples/macro-micro/macro-heat` and execute `./test_macro_heat`
-### Running the coupled simulation
+### Running the coupled Nutils(Micro)-DuMuX(Macro) simulation
 * Set the `RunWithCoupling ` parameter in `macro-heat/params.input`to `true`. Rebuild afterwards.
 * Open two separate shells. In one of them, run the macro simulation as above.
 * In the other, navigate to `dumux/dumux-adapter/build-cmake/examples/macro-micro/nutils-micro-heat` and execute `python3 run-micro-manager.py`.
 * To run the micro code in parallel, use `mpirun -n <NThreads> python3 run-micro-problems.py` instead.
+## Running the coupled DuMuX(Micro)-DuMuX(Macro) simulation
+* Open two separate shells, in one of them, run the macro simulation as above.
+* In the other, navigate to `dumux/dumux-adapter/build-cmake/examples/macro-micro/micro-heat`, then run the micro code as above.
 
 ## Choosing the parameters
 In order to modify the simulation parameters, modify `params.input`.
--- micro-heat/micro_sim.cpp
+++ micro-heat/micro_sim.cpp
@@ -5,10 +5,55 @@
 #include <pybind11/stl.h> // std::vector conversion
 #include <numbers> // std::numbers
 
-namespace py = pybind11;
+#include <config.h>
+
+#include <ctime>
+#include <iostream>
+#include <fstream>
+
+#include <dune/common/timer.hh>
+#include <dune/grid/io/file/vtk.hh>
+#include <dune/istl/io.hh>
+
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/dumuxmessage.hh>
+#include <dumux/common/defaultusagemessage.hh>
+
+#include <dumux/linear/seqsolverbackend.hh>
+#include <dumux/linear/pdesolver.hh>        // for LinearPDESolver
+#include <dumux/nonlinear/newtonsolver.hh>
+
+#include <dumux/assembly/fvassembler.hh>
+#include <dumux/assembly/diffmethod.hh>
+
+#include <dumux/discretization/method.hh>
+#include <dumux/io/grid/gridmanager.hh>
+#include <dumux/io/vtkoutputmodule.hh>
+
+#include "properties_allencahn.hh"
+#include "properties_cellproblem.hh"
+
+namespace py = pybind11; 
 
 class MicroSimulation
-{
+{   
+    using AllenCahnTypeTag = Dumux::Properties::TTag::PlainAllenCahn;
+    using CellProblemTypeTag = Dumux::Properties::TTag::CellProblem;
+    using ACSolutionVector = Dumux::GetPropType<AllenCahnTypeTag, Dumux::Properties::SolutionVector>;
+    using CPSolutionVector = Dumux::GetPropType<CellProblemTypeTag, Dumux::Properties::SolutionVector>;
+    using ACProblem = Dumux::GetPropType<AllenCahnTypeTag, Dumux::Properties::Problem>;
+    using CPProblem = Dumux::GetPropType<CellProblemTypeTag, Dumux::Properties::Problem>;
+    using ACGridVariables = Dumux::GetPropType<AllenCahnTypeTag, Dumux::Properties::GridVariables>;
+    using CPGridVariables = Dumux::GetPropType<CellProblemTypeTag, Dumux::Properties::GridVariables>;
+    using ACAssembler = Dumux::FVAssembler<AllenCahnTypeTag, Dumux::DiffMethod::numeric>;
+    using CPAssembler = Dumux::FVAssembler<CellProblemTypeTag, Dumux::DiffMethod::numeric>; 
+    using LinearSolver = Dumux::UMFPackBackend;
+    using ACNewtonSolver = Dumux::NewtonSolver<ACAssembler, LinearSolver>;
+    using CPNewtonSolver = Dumux::NewtonSolver<CPAssembler, LinearSolver>;
+    using GridGeometry = Dumux::GetPropType<AllenCahnTypeTag, Dumux::Properties::GridGeometry>;   //TODO care that works, is same for CP
+    using Scalar = Dumux::GetPropType<AllenCahnTypeTag, Dumux::Properties::Scalar>;
+
 public:
     MicroSimulation(int sim_id);
     void initialize();
@@ -30,14 +75,30 @@ private:
     double _k_11;
     double _porosity;
     double _checkpoint;
+
+    std::shared_ptr<CPNewtonSolver> _cpNonLinearSolver;
+    std::shared_ptr<ACNewtonSolver> _nonLinearSolver;
+    std::shared_ptr<ACAssembler> _acAssembler;
+    std::shared_ptr<CPAssembler> _cpAssembler;
+    std::shared_ptr<Dumux::CheckPointTimeLoop<double> > _timeLoop;
+    std::shared_ptr<ACProblem> _acProblem;
+    std::shared_ptr<CPProblem> _cpProblem;
+    std::shared_ptr<CPGridVariables> _cpGridVariables;
+    std::shared_ptr<ACGridVariables> _acGridVariables;
+    ACSolutionVector _phi;
+    std::shared_ptr<ACSolutionVector> _phiOldPtr;
+    CPSolutionVector _psi1;
+    CPSolutionVector _psi2;
 };
 
 // Constructor
-MicroSimulation::MicroSimulation(int sim_id) : _sim_id(sim_id), _dims(3), _k_00(0), _k_01(0), _k_10(0),_k_11(0),_porosity(0), _checkpoint(0) {}
+MicroSimulation::MicroSimulation(int sim_id) : _sim_id(sim_id), _dims(3), _k_00(0), _k_01(0), _k_10(0),_k_11(0),_porosity(0), _checkpoint(0) {};
 
 // Initialize
 void MicroSimulation::initialize()
-{
+{   
+    using namespace Dumux;
+
     std::cout << "Initialize micro problem (" << _sim_id << ")\n";
     //_micro_scalar_data = 0;
     _k_00 = 0;
@@ -46,11 +107,93 @@ void MicroSimulation::initialize()
     _k_11 = 0;
     //_micro_vector_data.clear();
     _checkpoint = 0;
+
+    // parse command line arguments and input file
+    Parameters::init("params.input");//argc, argv); TODO 
+
+    // try to create a grid (from the given grid file or the input file)
+    GridManager<GetPropType<AllenCahnTypeTag, Properties::Grid>> gridManager;
+    gridManager.init();
+
+    // we compute on the leaf grid view
+    const auto& leafGridView = gridManager.grid().leafGridView();
+
+    // create the finite volume grid geometry
+    auto gridGeometry = std::make_shared<GridGeometry>(leafGridView);
+    gridGeometry->update(leafGridView);
+
+    ////////////////////////////////////////////////////////////
+    // Set up the Allen-Cahn Problem
+    ////////////////////////////////////////////////////////////
+    
+    // the allen-cahn problem (initial and boundary conditions)
+    _acProblem = std::make_shared<ACProblem>(gridGeometry);
+
+    // get some time loop parameters
+    const auto tEnd = getParam<Scalar>("TimeLoop.TEnd");
+    const auto maxDt = getParam<Scalar>("TimeLoop.MaxTimeStepSize");
+    auto dt = getParam<Scalar>("TimeLoop.DtInitial");
+
+    // the solution vector
+    //TODO check
+    auto phiPtr = std::make_shared<ACSolutionVector>();
+    _phi = *phiPtr;
+    _acProblem->applyInitialSolution(_phi);
+    _phiOldPtr = std::make_shared<ACSolutionVector>();
+    *_phiOldPtr = _phi;
+
+    // the grid variables
+    _acGridVariables = std::make_shared<ACGridVariables>(_acProblem, gridGeometry);
+    _acGridVariables->init(_phi);
+
+    // instantiate time loop
+    _timeLoop = std::make_shared<CheckPointTimeLoop<Scalar>>(0.0, dt, tEnd);
+    _timeLoop->setMaxTimeStepSize(maxDt);
+
+    // the assembler with time loop for instationary problem
+    _acAssembler = std::make_shared<ACAssembler>(_acProblem, gridGeometry, _acGridVariables, _timeLoop, *_phiOldPtr);
+
+    // the linear solver
+    auto linearSolver = std::make_shared<LinearSolver>();
+
+    // the non-linear solver
+    _nonLinearSolver = std::make_shared<ACNewtonSolver>(_acAssembler, linearSolver);
+
+    //TODO: solve one step of AC to init cell problem
+
+    ////////////////////////////////////////////////////////////
+    // Set up the Cell Problem
+    ////////////////////////////////////////////////////////////
+
+    //setup the cell problem
+    _cpProblem = std::make_shared<CPProblem>(gridGeometry);
+
+    // the solution vectors
+    //using CPSolutionVector = GetPropType<CellProblemTypeTag, Properties::SolutionVector>;
+    CPSolutionVector psi1(leafGridView.size(0));
+    _psi1 = psi1;
+    CPSolutionVector psi2(leafGridView.size(0));
+    _psi2 = psi2;
+
+    // the grid variables
+    _cpGridVariables = std::make_shared<CPGridVariables>(_cpProblem, gridGeometry);
+    _cpGridVariables->init(_psi1);
+
+    // TODO the assembler with time loop for stationary problem
+    // (using nonlinear solver for now, should be linear)
+    _cpAssembler = std::make_shared<CPAssembler>(_cpProblem, gridGeometry, _cpGridVariables);
+    //LinearPDESolver<CPAssembler, LinearSolver> cpSolver(cpAssembler, linearSolver);
+    
+    // the non-linear solver REPLACE
+    _cpNonLinearSolver = std::make_shared<CPNewtonSolver>(_cpAssembler, linearSolver);
+
+    // intialize the vtk output module
+    _timeLoop->start();
 }
 
 // Solve
 py::dict MicroSimulation::solve(py::dict macro_write_data, double dt)
-{
+{   
     std::cout << "Solve timestep of micro problem (" << _sim_id << ")\n";
 
     // assert(dt != 0);
@@ -59,14 +202,69 @@ py::dict MicroSimulation::solve(py::dict macro_write_data, double dt)
         std::cout << "dt is zero\n";
         exit(1);
     }
+    
+    //_timeLoop->setTimeStepSize(dt); TODO
 
     //! Here, insert your code, changing the data and casting it to the correct type
     // create double variable from macro_write_data["micro_scalar_data"]; which is a python float
     //double macro_scalar_data = macro_write_data["macro-scalar-data"].cast<double>();
     double conc = macro_write_data["concentration"].cast<double>();
 
-    //TODO set _k_00 etc
-    _k_00 = conc; //TODO remove
+    std::cout << "CHECK A" << std::endl;
+
+    //input macro concentration into allen-cahn problem
+    _acProblem->updateConcentration(conc);
+
+    std::cout << "CHECK B" << std::endl;
+    std::cout << "CHECK _PHI.size = " <<_phi.size() << std::endl;
+   
+
+    // linearize & solve the allen cahn problem
+    _nonLinearSolver->solve(_phi, *_timeLoop);
+
+    std::cout << "CHECK C" << std::endl;
+
+    //calculate porosity 
+    _porosity = _acProblem->calculatePorosity(_phi);
+
+    std::cout << "CHECK D" << std::endl;
+
+    //update Phi in the cell problem
+    _cpProblem->spatialParams().updatePhi(_phi);
+
+    //solve the cell problems 
+    std::cout << "Solve Cell Problem 1" << std::endl;
+    int psiIdx = 0;
+    _cpProblem->spatialParams().updatePsiIndex(psiIdx);
+    _cpGridVariables->update(_psi1);
+    _cpNonLinearSolver->solve(_psi1); 
+    std::cout << "Solve Psi Derivative" << std::endl;
+    _cpProblem->computePsiDerivatives(*_cpProblem, *_cpAssembler, *_cpGridVariables, _psi1, psiIdx);
+
+    std::cout << "Solve Cell Problem 2" << std::endl;
+    psiIdx = 1;
+    _cpProblem->spatialParams().updatePsiIndex(psiIdx);
+    _cpGridVariables->update(_psi2);
+    _cpNonLinearSolver->solve(_psi2); 
+    std::cout << "Solve Psi Derivative" << std::endl;
+    _cpProblem->computePsiDerivatives(*_cpProblem, *_cpAssembler, *_cpGridVariables, _psi2, psiIdx);
+
+    //calculate the conductivity tensor 
+    _k_00 = _cpProblem->calculateConductivityTensorComponent(0,0);
+    _k_10 = _cpProblem->calculateConductivityTensorComponent(1,0);
+    _k_01 = _cpProblem->calculateConductivityTensorComponent(0,1);
+    _k_11 = _cpProblem->calculateConductivityTensorComponent(1,1);
+
+    // make the new solution the old solution
+    *_phiOldPtr = _phi;
+    _acGridVariables->advanceTimeStep();
+
+    // advance to the time loop to the next step
+    _timeLoop->advanceTimeStep();
+
+    // report statistics of this time step
+    _timeLoop->reportTimeStep();
+
     // create python dict for micro_write_data
     py::dict micro_write_data;
     // add micro_scalar_data and micro_vector_data to micro_write_data
